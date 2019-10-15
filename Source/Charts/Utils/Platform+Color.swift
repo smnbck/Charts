2f96d25e1985f130d4ad558e0377033dd0bc53bb
Jacob Christie <jacob.christie@kinduct.com>
2019-10-15 13:26:58 -0300

    Removed unnecessary platform accommodations

--- Source/Charts/Utils/Platform+Color.swift
+++ Source/Charts/Utils/Platform+Color.swift
@@ -7,22 +7,17 @@
 
 #if canImport(UIKit)
 import UIKit
-
 public typealias NSUIColor = UIColor
-extension UIColor {
-    @available(iOS, introduced: 8.0, obsoleted: 13.0)
-    @available(tvOS, introduced: 9.0, obsoleted: 13.0)
-    static var label: UIColor { .black }
-}
 #endif
 
 #if canImport(AppKit)
-
 import AppKit
-
 public typealias NSUIColor = NSColor
-extension NSColor {
-    @available(macOS, introduced: 10.11, obsoleted: 10.14)
-    static var label: NSColor { .black }
-}
 #endif
+
+extension NSUIColor {
+    @available(iOS, introduced: 8.0, obsoleted: 13.0)
+    @available(tvOS, introduced: 9.0, obsoleted: 13.0)
+    @available(macOS, introduced: 10.11, obsoleted: 10.14)
+    static var label: NSUIColor { .black }
+}
